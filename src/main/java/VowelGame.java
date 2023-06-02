812691d3e7e3d99c53690947a1fcd6bfa3a68333
Cverkade <verkadecornelis@gmail.com>
2023-06-02 11:37:56 +0200

    working properly

--- src/main/java/VowelGame.java
+++ src/main/java/VowelGame.java
@@ -1,20 +1,18 @@
-import java.util.Arrays;
-
 public class VowelGame {
 
     public static void main (String args[]){
-       transform("mms", "sms");
+       transform("black", "white");
     }
+
     public static String[] transform(String str1, String str2) {
 
-        if (!str1.contains("(?<=[aeiou])") || str2.contains("(?<=[aeiou])")){
+        if (!str1.matches(".*[aeiou].*") || !str2.matches(".*[aeiou].*")){
             throw new IllegalArgumentException("Error");
         }
 
         String[] result1 = str1.split("(?<=[aeiou])", 2);
         String[] result2 = str2.split("(?<=[aeiou])", 2);
         String[] result = {result1[0] + result2[1], result2[0] + result1[1]};
-        Arrays.stream(result).forEach(System.out::println);
 
         return result;
     }
